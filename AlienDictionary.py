de30da7acb2bb286d536ec0b2e08cde0cbfa8f58
Adam Harner <adamj.harner@gmail.com>
2020-01-06 12:50:25 -0500

    Uploaded solution
    Solution for LC # 953

---
+++ AlienDictionary.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        #give index of particular chars in the Alien Dict
+        words_values = [[order.index(char) for char in list(word)] for word in words]
+        
+        while len(words_values) > 1:
+            #compare 1st word to 2nd and if it's before, remove it from list. When 1 word left, all words are in order
+            
+            for i in range(len(words_values[0])):
+                if words_values[0][i] > words_values[1][i]:
+                    return False
+                elif words_values[0][i] < words_values[1][i]:
+                    words_values = words_values[1:]
+                    break
+                
+                if i == len(words_values[0]) -1:
+                    words_values = words_values[1:]
+                elif i == len(words_values[1]) - 1:
+                    return False
+                
+            return True
