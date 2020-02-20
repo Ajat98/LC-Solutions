80aa592c24bd6af6fa9ccbb19a52e64425719224
Adam Harner <adamj.harner@gmail.com>
2020-02-20 15:18:40 -0500

    Added simpler solution.

--- DominantIndex.py
+++ DominantIndex.py
@@ -22,3 +22,19 @@ class Solution:
         else:
             return place
         
+ #Another simple Solution, O(n) time and O(1) space.
+ #compare the largest number with 2x the second largest number.
+	    max1 = 0
+        max2 = 0
+        
+        for num in nums:
+            if num > max1:
+                max2 = max1
+                max1 = num
+            elif num > max2:
+                max2 = num
+                
+        if max1 >= max2*2:
+            return nums.index(max1)
+        else:
+            return -1
