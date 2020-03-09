eba15be5311042a5c4f83c1bb92559c4b8c391bd
Adam Harner <adamj.harner@gmail.com>
2020-03-09 12:48:14 -0400

    Updated single_num.py

--- single_num.py
+++ single_num.py
@@ -4,12 +4,26 @@
 
 #Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
-
-
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        #Using XOR values
+        """
         result = 0
         for n in nums:
             result ^= n #any num XOR'd with itself is 0, so duplicates will cancel out.
-            print(result)
+
         return result
+        """
+    
+        #Using hash map
+        d = {}
+    
+        for i in nums:
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i] +=1
+        print(d)
+        for k in d:
+            if d[k] < 2:
+                return k
